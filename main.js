c2fcf7ded005082ce7862c1530b140d5fd569585
jdelacruzv <jldlcv@gmail.com>
2021-02-20 17:45:41 -0500

    Fix in dropdown menu links

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 // ---------------> Efecto del header cuando se hace scroll --------------->
+
 let hiden_name = document.querySelector('.hidden-name');
 
 function showScroll() {
@@ -7,7 +8,7 @@ function showScroll() {
     // Distancia de la ventana hacia el elemento
     let distance = hiden_name.offsetTop;
 
-    if((distance + 620) < scroll_top) {
+    if((distance + 450) < scroll_top) {
         hiden_name.style.opacity = 1;
     }
     else {
@@ -19,53 +20,39 @@ window.addEventListener('scroll', showScroll);
 
 
 // ---------------> Efecto menu hamburguesa --------------->
+
 let btn_menu = document.querySelector('.btn-menu');
 let menu_items = document.querySelector('.menu-items');
+let menu_links = document.querySelectorAll('a.menu-link');
 
 btn_menu.addEventListener('click', () => {
     btn_menu.classList.toggle('active');
     menu_items.classList.toggle('show');
 });
 
+// El menu desaparece cuando se hace click a cada enlace
+menu_links.forEach(link => {
+    link.addEventListener('click', () => {
+        menu_items.classList.remove('show');
+        btn_menu.classList.toggle('active');
+    });
+});
+
 
 // ---------------> Ventana no modal hoja de vida (resume) --------------->
-let window_resume = document.getElementById("window-resume");
-let body = document.querySelector('body');
-let resume = document.getElementById("resume");
-let close_resume = document.getElementById("close-resume");
 
-resume.addEventListener("click", () => {
-    window_resume.classList.add("show-modal");
+let resume = document.getElementById('resume');
+let window_resume = document.getElementById('window-resume');
+let body = document.querySelector('body');
+let close_resume = document.getElementById('close-resume');
+
+resume.addEventListener('click', () => {
+    window_resume.classList.add('show-modal');
     body.style.overflow = 'hidden';
 
 });
 
-close_resume.addEventListener("click", () => {
-    window_resume.classList.remove("show-modal")
+close_resume.addEventListener('click', () => {
+    window_resume.classList.remove('show-modal')
     body.style.overflow = 'visible';
 });
-
-
-// ---------------> Escucha el evento si esta en true o false --------------->
-// Captura todos los medias queries que son de 768px para abajo
-// let changes = window.matchMedia('screen and (max-width:768px)');
-// changes.addEventListener('click', validation);
-// validation(changes);
-
-// function validation(event) {
-//     if(event.matches) {
-//         btn_menu.addEventListener('click', watchHidden);
-//     }
-//     else {
-//         btn_menu.removeEventListener('click', watchHidden);
-//     }
-// }
-
-// function watchHidden() {
-//     if(menu_items.classList.contains('show')) {
-//         menu_items.classList.remove('show');
-//     }
-//     else {
-//         menu_items.classList.add('show');
-//     }
-// }
