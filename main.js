f41ead90c90513054c177cc791bd05979f446157
jdelacruzv <jldlcv@gmail.com>
2021-05-19 21:48:23 -0500

    Main modified

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const hiden_name = document.querySelector('.hidden-name');
+const hidden_par = document.querySelector('.hidden-par');
+const header = document.querySelector('.header');
 const btn_menu = document.querySelector('.btn-menu');
 const menu_items = document.querySelector('.menu-items');
 const menu_links = document.querySelectorAll('a.menu-link');
@@ -7,22 +8,26 @@ const window_resume = document.getElementById('window-resume');
 const body = document.querySelector('body');
 const close_resume = document.getElementById('close-resume');
 
+
 // ---------------> Efecto del header cuando se hace scroll --------------->
 function showScroll() {
     // Detecta la cantidad de scroll cuando se esta bajando
     let scroll_top = document.documentElement.scrollTop;
     // Distancia de la ventana hacia el elemento
-    let distance = hiden_name.offsetTop;
+    let distance = hidden_par.offsetTop;
 
     if((distance + 450) < scroll_top) {
-        hiden_name.style.opacity = 1;
+        hidden_par.style.opacity = 1;
+        header.style.opacity = 1;
     }
     else {
-        hiden_name.style.opacity = 0;
+        hidden_par.style.opacity = 0;
+        header.style.opacity = 0;
     }
 }
 
 window.addEventListener('scroll', showScroll);
+
 
 // ---------------> Efecto menu hamburguesa --------------->
 btn_menu.addEventListener('click', () => {
@@ -37,6 +42,7 @@ menu_links.forEach(link => {
         btn_menu.classList.toggle('active');
     });
 });
+
 
 // ---------------> Ventana no modal hoja de vida (resume) --------------->
 resume.addEventListener('click', () => {
