a99d31893fb093f994bd5c4428c2338848b71131
jdelacruzv <jldlcv@gmail.com>
2021-05-31 21:56:13 -0500

    Section about me was added

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-const menu_toggle = document.getElementById('menu-toggle');
-const navbar = document.getElementById('navbar');
+const navbar_toggle = document.getElementById('navbar__toggle');
+const navbar_list = document.getElementById('navbar__list');
 const navbar_links = document.querySelectorAll('a.navbar__link');
 
 //  Menu toggle 
-menu_toggle.addEventListener('click', () => {
-    navbar.classList.toggle('show');
+navbar_toggle.addEventListener('click', () => {
+    navbar_list.classList.toggle('show');
 });
 
 // El menu desaparece cuando se hace click a cada enlace
 navbar_links.forEach(link => {
     link.addEventListener('click', () => {
-        navbar.classList.remove('show');
+        navbar_list.classList.remove('show');
     });
 });
