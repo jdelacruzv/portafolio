5555fc8a59c6b46233bcc9e098e88bad33f1714c
jdelacruzv <jldlcv@gmail.com>
2021-06-01 21:02:00 -0500

    Normalize.css file was added

--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@ const navbar_links = document.querySelectorAll('a.navbar__link');
 
 //  Menu toggle 
 navbar_toggle.addEventListener('click', () => {
-    navbar_list.classList.toggle('show');
+  navbar_list.classList.toggle('show');
 });
 
 // El menu desaparece cuando se hace click a cada enlace
 navbar_links.forEach(link => {
-    link.addEventListener('click', () => {
-        navbar_list.classList.remove('show');
-    });
+  link.addEventListener('click', () => {
+    navbar_list.classList.remove('show');
+  });
 });
