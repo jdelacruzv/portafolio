eeee594b01d6029221c29a722abf7cd56b5b48c1
Jose De La Cruz <jldlcv@gmail.com>
2022-10-12 15:51:08 +0200

    added social media icons

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -3,29 +3,69 @@ import style from "./AboutPage.module.css";
 const AboutPage = () => {
 	return (
 		<section className={style.about__container}>
-			<figure className={style.about__photo}>
-				<img
-					className={style.about__img}
-					src="/assets/images/family.png"
-					alt="Photo family"
-				/>
-			</figure>
-			<div className={style.about__texts}>
-				<p className={style.about__text}>
-					Soy administrador de empresas y profesional técnico en computación e
-					informática con experiencia en el área de logística. Nací en Perú, viví y
-					actualmente resido en Colombia.
-				</p>
-				<p className={style.about__text}>
-					Me considero autodidacta y apasionada de la tecnología; a mediados del 2020
-					y en medio de la crisis sanitaria, tomé la decisión de cambiar de carrera
-					para aprender desarrollo web.
-				</p>
-				<p className={style.about__text}>
-					Me gusta pasar mucho tiempo compartiendo con mi familia, viendo películas
-					de acción, ciencia ficción y documentales; de vez en cuando disfruto
-					cocinar. 😉
-				</p>
+			<article className={style.aboutme__container}>
+				<figure className={style.aboutme__photo}>
+					<img
+						className={style.aboutme__img}
+						src="/assets/images/me3.jpeg"
+						alt="Photo family"
+					/>
+				</figure>
+				<div className={style.aboutme__texts}>
+					<p className={style.aboutme__text}>
+						I am a business administrator and technical professional in computer
+						science with experience in the logistics area. I was born in Peru, I lived
+						in Colombia for almost 4 years and since 2022 I have lived in Spain.
+					</p>
+					<p className={style.aboutme__text}>
+						I consider myself self-taught and passionate about technology; In the
+						middle of 2020 and in the midst of the global health crisis, I made the
+						decision to change careers to learn web development.
+					</p>
+					<p className={style.aboutme__text}>
+						I like to spend a lot of time sharing with my family, watching action
+						movies, science fiction and documentaries. From time to time I enjoy
+						cooking. 😉
+					</p>
+				</div>
+			</article>
+			<div className={style.social}>
+				<a
+					className={style.social__link}
+					href="https://www.facebook.com/joseluis.delacruz.vela/"
+					target="_blank"
+				>
+					<img
+						id="social__facebook"
+						className={style.social__img}
+						src="assets/icons/facebook.png"
+						alt="Logo facebook"
+					/>
+				</a>
+				<a
+					className={style.social__link}
+					href="https://twitter.com/jose_delacruz_v/"
+					target="_blank"
+				>
+					<img
+						id="social__twitter"
+						className={style.social__img}
+						src="assets/icons/twitter.png"
+						alt="Logo twitter"
+					/>
+				</a>
+				<a
+					className={style.social__link}
+					href="https://www.instagram.com/jose_delacruz_v/"
+					target="_blank"
+				>
+					<img
+						id="social__instagram"
+						className={style.social__img}
+						src="assets/icons/instagram.png"
+						alt="Logo instagram"
+					/>
+				</a>
 			</div>
 		</section>
 	);
