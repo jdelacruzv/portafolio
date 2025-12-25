cabb15608c8c9c6bad5ce7c388112340a476214e
Jose De La Cruz <jldlcv@gmail.com>
2025-12-25 15:54:43 +0100

    refactoring and centering of the aboutpage

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -3,33 +3,27 @@ import style from "./AboutPage.module.css";
 const AboutPage = () => {
 	return (
 		<section className={style.about__container}>
-			<article className={style.aboutme__container}>
-				<figure className={style.aboutme__photo}>
-					<img
-						className={style.aboutme__img}
-						src="/assets/images/me.jpeg"
-						alt="Photo Jose"
-					/>
-				</figure>
-				<div className={style.aboutme__texts}>
-					<p className={style.aboutme__text}>
-						I am a business administrator and technical professional in computer
-						science with experience in the logistics area. I was born in Peru, I lived
-						in Colombia for almost 4 years and since 2022 I have lived in Spain.
-					</p>
-					<p className={style.aboutme__text}>
-						I consider myself self-taught and passionate about technology; In the
-						middle of 2020 and in the midst of the global health crisis, I made the
-						decision to change careers to learn web development.
-					</p>
-					<p className={style.aboutme__text}>
-						I like to spend a lot of time sharing with my family, watching action
-						movies, science fiction and documentaries. From time to time I enjoy
-						cooking. 😉
-					</p>
-				</div>
-			</article>
+			<figure className={style.about__photo}>
+				<img
+					className={style.about__img}
+					src="/assets/images/jose.jpg"
+					alt="Photo Jose"
+				/>
+			</figure>
+			<div className={style.about__data}>
+				<p className={style.about__text}>
+					I am a business administrator and technical professional in computer
+					science with experience in the logistics area. I was born in Peru, I lived
+					in Colombia for almost 4 years and since 2022 I have lived in Spain.
+				</p>
+				<p className={style.about__text}>
+					I consider myself self-taught and passionate about technology; In the
+					middle of 2020 and in the midst of the global health crisis, I made the
+					decision to change careers to learn web software development (fullstack).
+				</p>
+			</div>
 		</section>
+		// </section>
 	);
 };
 
