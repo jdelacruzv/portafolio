42954ae41f4fc820719cfe8900f93ff2ae9b0bc7
Jose De La Cruz <jldlcv@gmail.com>
2026-02-01 20:36:00 +0100

    refactoring homePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,27 +3,27 @@ import Skills from "../components/Skills";
 import style from "./HomePage.module.css";
 import { icons } from "../utils/icons";
 
-const showIcons = icons.map((icon) => <Skills key={icon.id} icon={icon.name}/>);
+const showIcons = icons.map((icon) => (
+	<Skills key={icon.id} icon={icon.name} />
+));
 
 const HomePage = () => {
 	return (
 		<section className={style.home__container}>
-			<div className={style.home__data}>
-				<h1 className={style.home__title}>
-					<p className={style.home__title__white}>I am a</p>
-					<p className={style.home__title__color}>Software developer</p>
-					<p className={style.home__title__text}>
-						Hi, my name is <span className={style.home__text__color}>JOSE </span>
-						and I invite you to take a look at some of my projects.
-					</p>
-				</h1>
-				<Button 
-					text={"Projects"}	
-					icon={"/assets/icons/next.png"} 
-					path={"/projects"}
-				/>
-				<div className={style.home__icons}>{showIcons}</div>
-			</div>
+			<h1 className={style.home__title}>
+				<p className={style.home__title__white}>I am a</p>
+				<p className={style.home__title__color}>Software developer</p>
+				<p className={style.home__title__text}>
+					Hi, my name is <a href="/about">JOSE</a> and I invite you to take a look at
+					some of my projects.
+				</p>
+			</h1>
+			<Button
+				text={"Projects"}
+				icon={"/assets/icons/next.png"}
+				path={"/projects"}
+			/>
+			<div className={style.home__icons}>{showIcons}</div>
 		</section>
 	);
 };
